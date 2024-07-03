9ba368fc6b624afe33fa50a1fba393bc79752388
Rajkumarchirumamilla <rk@sltechnocrats.net>
2024-07-03 16:36:47 +0530

    hhh

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use("/api/Withdrawl", WithdrawlRouter);
 app.use("/api/History", HistoryRouter);
 app.use("/api/Create", UserRouter);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
